44bb72094a140542bc7281f9dea7d0baf065eab3
Osank31 <osankverma2004@gmail.com>
2024-12-02 00:45:35 +0530

    getting the click coordinates error

--- flyMusquito.js
+++ flyMusquito.js
@@ -1,10 +1,45 @@
 let clickCoordinates=null;
+const videoElement = document.getElementById('inputVideo');
+const canvasElement = document.getElementById('outputCanvas');
+canvasElement.width=1920;
+canvasElement.height=1080;
+const gameCanvas = document.getElementById("gameCanvas");
+const gameCanvasCtx = gameCanvas.getContext("2d");
+gameCanvas.width = 1280;
+gameCanvas.height = 720;
+const openHandImage = document.getElementById("openHand")
+const closedHandImage=document.getElementById("closedHand");
+import MediapipeHands from "./mediapipe.js";
+
+const ms = new MediapipeHands(videoElement, canvasElement, openHandImage,closedHandImage, gameCanvas, gameCanvasCtx);
+ms.initialize();
+ms.click = function (lmList, results, positionCoordinates) {
+    const handState = this.isHandClosed(lmList, results);
+    let click = false;
+
+    if (!this.isClosed && handState === "HandClosed") {
+        this.isClosed = true;
+        click = true;
+    } else if (this.isClosed && handState === "HandOpen") {
+        this.isClosed = false;
+    }
+
+    if (click) {
+        console.log("Click detected at position:", positionCoordinates);
+        //------>
+
+        // clickCoordinates={x: positionCoordinates[0], y: positionCoordinates[1]};
+        // console.log(clickCoordinates)
+
+    }
+};
 
 gameCanvas.addEventListener('click', function(event) {
     const rect = gameCanvas.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
-    clickCoordinates={x,y};
+    // clickCoordinates={x,y};
+    console.log(x,y)
 });
 
 export function flyMusquito(gameCanvas, gameCanvasCtx,musquitoImage, noOfMusquitoes) {
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-import MediapipeHands from "./mediapipe.js";
+// import MediapipeHands from "./mediapipe.js";
 import { flyMusquito } from "./flyMusquito.js";
-const videoElement = document.getElementById('inputVideo');
+// const videoElement = document.getElementById('inputVideo');
 const canvasElement = document.getElementById('outputCanvas');
 canvasElement.width=1920;
 canvasElement.height=1080;
@@ -9,31 +9,33 @@ const gameCanvasCtx = gameCanvas.getContext("2d");
 gameCanvas.width = 1280;
 gameCanvas.height = 720;
 const musquitoImage = document.getElementById("musquitoImage");
-const openHandImage = document.getElementById("openHand")
-const closedHandImage=document.getElementById("closedHand");
+// const openHandImage = document.getElementById("openHand")
+// const closedHandImage=document.getElementById("closedHand");
+// let clickCoordinates=null;
 
-const ms = new MediapipeHands(videoElement, canvasElement, openHandImage,closedHandImage, gameCanvas, gameCanvasCtx);
-ms.initialize()
+// const ms = new MediapipeHands(videoElement, canvasElement, openHandImage,closedHandImage, gameCanvas, gameCanvasCtx);
+// ms.initialize()
 
-ms.click = function (lmList, results, positionCoordinates) {
-    const handState = this.isHandClosed(lmList, results);
-    let click = false;
+// ms.click = function (lmList, results, positionCoordinates) {
+//     const handState = this.isHandClosed(lmList, results);
+//     let click = false;
 
-    if (!this.isClosed && handState === "HandClosed") {
-        this.isClosed = true;
-        click = true;
-    } else if (this.isClosed && handState === "HandOpen") {
-        this.isClosed = false;
-    }
+//     if (!this.isClosed && handState === "HandClosed") {
+//         this.isClosed = true;
+//         click = true;
+//     } else if (this.isClosed && handState === "HandOpen") {
+//         this.isClosed = false;
+//     }
 
-    if (click) {
-        console.log("Click detected at position:", positionCoordinates);
-        //------>
+//     if (click) {
+//         console.log("Click detected at position:", positionCoordinates);
+//         //------>
 
-        
-    }
-};
+//         clickCoordinates={x: positionCoordinates[0], y: positionCoordinates[1]};
 
-let clickCoordinates=null;
+
+//     }
+// };
+
 
 flyMusquito(gameCanvas,gameCanvasCtx,musquitoImage, 5);
